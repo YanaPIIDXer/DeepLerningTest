df6861c6c3ba79d7968129016a01ec76e594a122
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-03-02 06:14:40 +0900

    関数名変更

--- perceptron.py
+++ perceptron.py
@@ -1,14 +1,14 @@
 import numpy as np
 
-def and_func(x1, x2):
+def and_gate(x1, x2):
     x = np.array([x1, x2])
     w = np.array([0.5, 0.5])
     bias = -0.7
     if (np.sum(x * w) + bias) > 0: return 1
     return 0
 
-print("=== and_func ===")
-print(and_func(0, 0))
-print(and_func(0, 1))
-print(and_func(1, 0))
-print(and_func(1, 1))
+print("=== AND ===")
+print(and_gate(0, 0))
+print(and_gate(0, 1))
+print(and_gate(1, 0))
+print(and_gate(1, 1))
