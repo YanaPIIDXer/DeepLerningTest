143ddb2831a495beca445ce0e0f918fd01072106
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-03-02 06:18:52 +0900

    ORゲート、NANDゲートを実装

--- perceptron.py
+++ perceptron.py
@@ -7,8 +7,34 @@ def and_gate(x1, x2):
     if (np.sum(x * w) + bias) > 0: return 1
     return 0
 
+def or_gate(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([0.5, 0.5])
+    bias = -0.2
+    if (np.sum(x * w) + bias) > 0: return 1
+    return 0
+
+def nand_gate(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([-0.5, -0.5])
+    bias = 0.7
+    if (np.sum(x * w) + bias) > 0: return 1
+    return 0
+
 print("=== AND ===")
 print(and_gate(0, 0))
 print(and_gate(0, 1))
 print(and_gate(1, 0))
 print(and_gate(1, 1))
+
+print("=== OR ===")
+print(or_gate(0, 0))
+print(or_gate(0, 1))
+print(or_gate(1, 0))
+print(or_gate(1, 1))
+
+print("=== NAND ===")
+print(nand_gate(0, 0))
+print(nand_gate(0, 1))
+print(nand_gate(1, 0))
+print(nand_gate(1, 1))
